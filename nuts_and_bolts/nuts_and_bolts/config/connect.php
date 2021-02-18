5eeacc838c62bd1e05a269fed8948d22d495174a
IanLarabee <61715399+IanLarabee@users.noreply.github.com>
2021-02-17 22:27:49 -0500

    Delete nuts_and_bolts/nuts_and_bolts directory
    Accidental directory creation

--- nuts_and_bolts/nuts_and_bolts/config/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $conn = sqlsrv_connect('aa10tpi5v27slik.cp9sea0sr7fk.us-east-1.rds.amazonaws.com, 1433', 'TeamOrangeB01', 'qyYgesGxH*Zb', 'NutsAndBoltsDB');
-
-    if(!$conn) {
-        die(print_r(sqlsrv_errors(), true));
-    }
-?>
--- nuts_and_bolts/nuts_and_bolts/config/get.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include ("connect.php");
-	
-	if (!sqlsrv_query($conn, "UPDATE ButtonTime SET TimePressed='".date("Y-m-d H:i:s")."' WHERE UserId='".$_COOKIE["id"]."'")){
-        die(print_r(sqlsrv_errors(), true));
-    }
-    sqlsrv_close($conn);
-?>
