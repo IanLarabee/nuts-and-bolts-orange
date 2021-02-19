25e30627d586a027a1035f7724c69dd2775c5735
Jonah Young <jyoung128@student.cscc.edu>
2021-02-18 19:14:19 -0500

    add main stylesheet + text footer

--- nuts_and_bolts/include/header.php
+++ nuts_and_bolts/include/header.php
@@ -4,4 +4,5 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="js/functions.js"></script>
+        <link rel="stylesheet" href="css/main.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
