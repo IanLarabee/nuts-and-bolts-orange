d1cc7f2ca3b4758cebaeeaf9e48b2a4f0c5f3565
Jonah Young <jyoung128@student.cscc.edu>
2021-02-22 22:04:49 -0500

    add navigation bar

--- nuts_and_bolts/index.php
+++ nuts_and_bolts/index.php
@@ -1,11 +1,19 @@
 <?php require_once "include/header.php"; ?>
 
-<title>Nuts and Bolts | Home</title>
+<title>Home | Nuts and Bolts</title>
 
     </head>
     <body>
-        <div class="container">
-            <h1>Hello, world!</h1>
-        </div>
+    
+    <div class="container">
+        <h1>Nuts and Bolts</h1>
+
+        <nav id="nav">
+        <a href="index.php" class="left">Home</a>
+        <a href="" class="left">Products</a>
+        <a href="" class="left">FAQ</a>
+        <a href="" class="left">Contact Us</a>
+        </nav>
+    </div>
 
 <?php require_once "include/footer.php"; ?>
