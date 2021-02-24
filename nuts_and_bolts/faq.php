eb290216d528f777e65909e92dcffd49e9c88a84
IanLarabee <ilarabee@student.cscc.edu>
2021-02-24 14:51:46 -0500

    removed FAQandContactInfo, uploaded faq.php

---
+++ nuts_and_bolts/faq.php
@@ -0,0 +1,33 @@
+<?php require_once "include/header.php"; ?>
+
+	<title>FAQ | Nuts and Bolts</title>
+</head>
+<body>
+
+	<h1>Frequently Asked Questions (FAQ)</h1>
+
+	<h2>(FAQ)	What are Nuts and Bolts Hardware Store Hours?</h2>
+	<p>Monday - Friday 7 AM - 10 PM; Saturday 8 AM - 8 PM; Sunday 10 AM - 5 PM</p>
+
+	<h2>(FAQ)	How long does it typically take to replinsh an out of stock item?</h2>
+	<p>Varies on the item. We get inventory shipped in twice a week, so most items replenished weekly.</p>
+
+	<h2>(FAQ)	Do you sell fishing licenses?</h2>
+	<p>We sell fishing licenses Monday - Saturday and have fresh night crawlers and minows daily.</p>
+
+	<h2>(FAQ)	Do you dispose of customer's old paint?</h2>
+	<p>We do dispose of old paint, as long as it was originally purchased here.</p>
+
+	<h2>(FAQ)	What types of items do you recycle for customers?</h2>
+	<p>We recycle plastic bags, all types small home batteries that don't contain mercury, and cell phones.</p>
+
+	<h2>(FAQ)	Do you rent tools?</h2>
+	<p>We rent a wide variety of electric tools, such as drills, and outdoor tools, such as lawnmowers and chainsaws.</p>
+
+	<h2>(FAQ)	Do you fix lawnmowers?</h2> 
+	<p>We fix lawnmowers from March through October.</p>
+
+	<h2>(FAQ)	Do you make keys?</h2>
+	<p>We make keys on the spot and guarantee they work the first time.</p>
+	
+<?php require_once "include/footer.php"; ?>
