b4f32e69ecabf3ce52bdbae92ceece3a58a33126
Jonah Young <jyoung128@student.cscc.edu>
2021-02-23 23:31:53 -0500

    Connect all webpages via navbar + edit css

--- nuts_and_bolts/include/header.php
+++ nuts_and_bolts/include/header.php
@@ -5,4 +5,5 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="assets/functions.js"></script>
         <link rel="stylesheet" href="assets/main.css">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Oswald&display=swap" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
--- nuts_and_bolts/index.php
+++ nuts_and_bolts/index.php
@@ -2,18 +2,18 @@
 
 <title>Home | Nuts and Bolts</title>
 
-    </head>
-    <body>
-    
-    <div class="container">
-        <h1>Nuts and Bolts</h1>
+</head>
+<body>
 
-        <nav id="nav">
+<div class="container">
+    <h1>Nuts and Bolts</h1>
+
+    <nav id="nav">
         <a href="index.php" class="left">Home</a>
         <a href="" class="left">Products</a>
-        <a href="" class="left">FAQ</a>
-        <a href="" class="left">Contact Us</a>
-        </nav>
-    </div>
+        <a href="faq.php" class="left">FAQ</a>
+        <a href="contact.php" class="left">Contact Us</a>
+    </nav>
+</div>
 
 <?php require_once "include/footer.php"; ?>
