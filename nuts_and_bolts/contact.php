8df38674e26830853a9d6b8af923ad3cdd2a71b3
IanLarabee <ilarabee@student.cscc.edu>
2021-02-24 14:41:58 -0500

    uploaded products. updated contact, index, footer

---
+++ nuts_and_bolts/contact.php
@@ -0,0 +1,16 @@
+<?php require_once "include/header.php"; ?>
+
+    <title>Contact Us | Nuts and Bolts</title>
+
+</head>
+<body>
+    
+    <h1>Get In touch with us</h1>
+    <h2>Email</h2>
+    <p>Nutsandbolts@gmail.com</p>
+    <h2>Phone number</h2>
+    <p> +1 (614) 555-5555 </p>
+    <h2> Address </h2>
+    <p> 69 E Magon Blv, Columbus, TX 68734
+
+<?php require_once "include/footer.php"; ?>
--- nuts_and_bolts/index.php
+++ nuts_and_bolts/index.php
@@ -1,6 +1,6 @@
 <?php require_once "include/header.php"; ?>
 
-<title>Home | Nuts and Bolts</title>
+    <title>Home | Nuts and Bolts</title>
 
 </head>
 <body>
@@ -10,7 +10,7 @@
 
     <nav id="nav">
         <a href="index.php" class="left">Home</a>
-        <a href="" class="left">Products</a>
+        <a href="products.php" class="left">Products</a>
         <a href="faq.php" class="left">FAQ</a>
         <a href="contact.php" class="left">Contact Us</a>
     </nav>
