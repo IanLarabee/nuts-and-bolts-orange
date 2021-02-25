83c633b9baf85ac3500b1fdde8813e844488b00a
Jonah Young <jyoung128@student.cscc.edu>
2021-02-24 21:45:05 -0500

    recommit

---
+++ nuts_and_bolts/contact.php
@@ -0,0 +1,35 @@
+<?php require_once "include/header.php"; ?>
+
+    <title>Contact Us | Nuts and Bolts</title>
+
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class = "container">
+            <a class="navbar-brand" href="index.php">Nuts and Bolts</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" aria-current="page" href="index.php">Home</a>
+                    <a class="nav-link" href="products.php">Products</a>
+                    <a class="nav-link" href="faq.php">FAQ</a>
+                    <a class="nav-link active" href="contact.php">Contact Us</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    
+    <div class="container">
+        <h1>Get In touch with us</h1>
+        <h2>Email</h2>
+        <p>Nutsandbolts@gmail.com</p>
+        <h2>Phone number</h2>
+        <p> +1 (614) 555-5555 </p>
+        <h2> Address </h2>
+        <p> 69 E Magon Blv, Columbus, TX 68734</p>
+    </div>
+
+<?php require_once "include/footer.php"; ?>
---
+++ nuts_and_bolts/faq.php
@@ -0,0 +1,62 @@
+<?php require_once "include/header.php"; ?>
+
+	<title>FAQ | Nuts and Bolts</title>
+</head>
+<body>
+
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class = "container">
+            <a class="navbar-brand" href="index.php">Nuts and Bolts</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" aria-current="page" href="index.php">Home</a>
+                    <a class="nav-link" href="products.php">Products</a>
+                    <a class="nav-link active" href="faq.php">FAQ</a>
+                    <a class="nav-link" href="contact.php">Contact Us</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+	<div class="container">
+		<h1>Frequently Asked Questions (FAQ)</h1>
+
+		<h2>What are Nuts and Bolts Hardware Store hours?</h2>
+		<p>Monday - Friday 7 AM - 10 PM; Saturday 8 AM - 8 PM; Sunday 10 AM - 5 PM</p>
+
+		<h2>What is our return policy</h2>
+		<p>All returns accepted within 7 days of purchased with no questions asked.</p>
+
+		<h2>Our Story</h2>
+		<p>Founded in 1976, Nuts and Bolts was a product of hard work paying off during hard times. Nuts and Bolts merely started as a dream until store founder and former veteran James Augustus Deathblade was able to make it a reality. 
+		In his mid thirties working as a bartender and fixing his friends' motorcycles at the local VA by day, Deathblade loved working with his hands but ends weren't meeting; he needed a change. 
+		Over the course of five long years, James pooled together what he could and was finally able to launch Nuts and Bolts and hasn't looked back since. Nuts and Bolts keeps prices low so that everyone has a chance to work with their hands!</p>
+		
+		<!--
+		<h2>(FAQ)	How long does it typically take to replinsh an out of stock item?</h2>
+		<p>Varies on the item. We get inventory shipped in twice a week, so most items replenished weekly.</p>
+
+		<h2>(FAQ)	Do you sell fishing licenses?</h2>
+		<p>We sell fishing licenses Monday - Saturday and have fresh night crawlers and minows daily.</p>
+
+		<h2>(FAQ)	Do you dispose of customer's old paint?</h2>
+		<p>We do dispose of old paint, as long as it was originally purchased here.</p>
+
+		<h2>(FAQ)	What types of items do you recycle for customers?</h2>
+		<p>We recycle plastic bags, all types small home batteries that don't contain mercury, and cell phones.</p>
+
+		<h2>(FAQ)	Do you rent tools?</h2>
+		<p>We rent a wide variety of electric tools, such as drills, and outdoor tools, such as lawnmowers and chainsaws.</p>
+
+		<h2>(FAQ)	Do you fix lawnmowers?</h2> 
+		<p>We fix lawnmowers from March through October.</p>
+
+		<h2>(FAQ)	Do you make keys?</h2>
+		<p>We make keys on the spot and guarantee they work the first time.</p>
+		-->
+	</div>
+	
+<?php require_once "include/footer.php"; ?>
