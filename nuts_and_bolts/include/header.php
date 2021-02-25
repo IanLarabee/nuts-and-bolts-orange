e06273610f36de4fb28aef416fdd41d7c964a1b4
IanLarabee <ilarabee@student.cscc.edu>
2021-02-24 23:40:29 -0500

    Added main.css, changed file readability

--- nuts_and_bolts/include/header.php
+++ nuts_and_bolts/include/header.php
@@ -3,25 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="assets/functions.js"></script>
-        <link rel="stylesheet" href="assets/main.css">
         <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Oswald&display=swap" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-        <style>
-			
-            html {
-			  position: relative;
-			  min-height: 100%;
-			}
-			body {
-			  margin-bottom: 80px;
-			}
-			.footer {
-			  position: absolute;
-			  bottom: 0;
-			  width: 100%;
-			  height: 50px;
-			  line-height: 20px;
-			  background-color: #f5f5f5;
-			}
-		</style>
+		<link rel="stylesheet" href="assets/main.css">
+		<script src="assets/functions.js"></script>
