aa237209e0a8969137b659c0de0af2f18a2396ad
IanLarabee <ilarabee@student.cscc.edu>
2021-02-24 22:26:58 -0500

    Adjusted footer to stick instead of float/ removed mac files

--- nuts_and_bolts/include/header.php
+++ nuts_and_bolts/include/header.php
@@ -7,3 +7,21 @@
         <link rel="stylesheet" href="assets/main.css">
         <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Oswald&display=swap" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <style>
+			
+            html {
+			  position: relative;
+			  min-height: 100%;
+			}
+			body {
+			  margin-bottom: 80px;
+			}
+			.footer {
+			  position: absolute;
+			  bottom: 0;
+			  width: 100%;
+			  height: 50px;
+			  line-height: 20px;
+			  background-color: #f5f5f5;
+			}
+		</style>
