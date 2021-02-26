3ae4c4477ec211c60d0faebfdef2a5e0a0b8afda
Jonah Young <jyoung128@student.cscc.edu>
2021-02-26 12:55:24 -0500

    add inventory sql file + update from main

--- nuts_and_bolts/contact.php
+++ nuts_and_bolts/contact.php
@@ -8,7 +8,6 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class = "container">
                 <a class="navbar-brand" href="index.php">Nuts and Bolts</a>
-                <a class="navbar-brand" href="#">Nuts and Bolts</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
