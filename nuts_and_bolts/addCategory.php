dcbc7de2d8f8533f2be1d6b5cecf5e40717b8e44
Jonah Young <jyoung128@student.cscc.edu>
2021-05-04 21:32:13 -0400

    add brand logo

--- nuts_and_bolts/addCategory.php
+++ nuts_and_bolts/addCategory.php
@@ -65,7 +65,7 @@
 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
-                <a class="navbar-brand" href="index.php">Nuts and Bolts</a>
+                <a class="navbar-brand" href="index.php"><img src="assets/nutsandboltslogo.png" alt="Nuts and Bolts" id="logo"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
