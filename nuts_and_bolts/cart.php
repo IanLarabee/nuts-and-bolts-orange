0f3b6f5d8de3366b5cf45ce289247c0f203d676d
IanLarabee <ilarabee@student.cscc.edu>
2021-05-06 17:14:10 -0400

    Removed modal on cart

--- nuts_and_bolts/cart.php
+++ nuts_and_bolts/cart.php
@@ -157,25 +157,7 @@
 		<div class="container">
             <h1>Cart</h1>
 			
-			<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-				<div class="modal-dialog">
-					<div class="modal-content">
-						<div class="modal-header">
-							<h5 class="modal-title" id="staticBackdropLabel">Apply Discount</h5>
-							<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-						</div>
-						<div class="modal-body">
-							...
-						</div>
-						<div class="modal-footer">
-							<form action='cart.php' method='POST'>
-								<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-								<input class="btn btn-success" name="apply" type="submit" value="Apply">
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
+
 			
 			<?php
 			if(isset($_SESSION['cart']) && count($_SESSION['cart']) != 0) {
